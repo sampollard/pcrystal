19ae079ba126e2b07c081f23d4bb38604d584c5c
Sam Pollard <sam.d.pollard@gmail.com>
2014-05-07 16:03:53 -0700

    Adjusted search parameters for custom report. Got the protein_list.txt to work even if there are newlines and spaces

--- getPDBData.py
+++ getPDBData.py
@@ -1,7 +1,7 @@
 # Download the CSV of protein data for a given set of PDBIDs and download the
 # pdb ent files from wwpdb
 # Author: Sam Pollard
-# Last Modified: May 6, 2014
+# Last Modified: May 7, 2014
 
 import urllib2
 import subprocess
@@ -15,25 +15,29 @@ reportName = 'report.csv'
 customRptStart = "http://www.rcsb.org/pdb/rest/customReport?pdbids="
 PDBIDListFile = open('protein_list.txt', 'r')
 PDBIDList = PDBIDListFile.read()
-# PDBIDList = "3G73,3L2C,3CO6,1VDE,1AF5,1EVX,2OST,1I50,4C2M,1QQC," +\
-# 			"1KN9,3BF0,4GWM,3VMT,2XCI,2OLV,4HHS,1CX2,1PRH,2LNL," +\
-# 			"4J5F,3I27,3P1Z,2HIV,2E2W,3L2P,2NSF,1FW1,2Z6W,4J4N," +\
-# 			"2WXW,4EXO,1ET1,3TH5,3TVD,2CLS,3KKV,4FG7,3RP9,3RDJ," +\
-# 			"1PV6,1PW4,3O7Q,3QS5,3QS4,3G61,3B5W,4FI3,1F30,1KJU"
+PDBIDList = PDBIDList.rstrip() # Remove all whitespace (including \n)
 customRptColumns = "&customReportColumns=structureTitle,experimentalTechnique"+\
 		",releaseDate,classification,structureMolecularWeight,chainLength," +\
 		"secondaryStructure,ALL,crystallizationMethod,crystallizationTempK," +\
 		"phValue,densityMatthews,densityPercentSol,pdbxDetails"
+
+customRptColumns = "&customReportColumns="+\
+		"structureTitle,experimentalTechnique,classification,compound,"+\
+		"expressionHost,"+\
+		"structureMolecularWeight,residueCount,chainLength,"+\
+		"ALL,crystallizationTempK,phValue,densityMatthews,densityPercentSol,pdbxDetails,"+\
+		"crystallizationMethod,spaceGroup,macromoleculeType,secondaryStructure"
+
 customRptEnd = "&service=wsdisplay&format=csv&ssa=n"
 reporturl = customRptStart + PDBIDList + customRptColumns + customRptEnd
 print reporturl
 # Run this only when needed
-if False:
+if True:
 	reqReport = urllib2.Request(reporturl)
 	print "Getting custom report from RCSB..."
 	reportResponse = urllib2.urlopen(reqReport)
 	results = reportResponse.read()
-	results.replace('<br />', '\n')
+	results = results.replace('<br />', '\n')
 	print "Writing custom report to "+ PDBDir + reportName
 	outfile = open(PDBDir+reportName, 'w')
 	outfile.write(results)
