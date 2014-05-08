9740c0529ef0dfeb98a773ce521c891094d86757
Sam Pollard <sam.d.pollard@gmail.com>
2014-05-08 00:36:03 -0700

    Got the R script to work. It now outputs the correct CSV of helices and sheets.

--- pdb.R
+++ pdb.R
@@ -2,7 +2,7 @@
 
 # the PDB file's HELIX and SHEET records.
 # Author: Clinton Burkhart
-# Modifed By Sam Pollard on May 7, 2014
+# Modifed By Sam Pollard on May 8, 2014
 
 # Load the Bio3D library that makes it easy for us to parse
 library("bio3d")
@@ -24,27 +24,24 @@ file.create(file = outfile)
 # Get a list of protein IDS from infile.
 # We are using a comma-separated file with no spaces between each PID
 pids <- strsplit(readLines(infile), ",", fixed = TRUE) #"[[:space:]]+"
-
+pids <- pids[[1]]
 # Write the header to the outfile.
-cat("Protein,% alpha helix,% beta sheet,", file = outfile, append = TRUE)
+cat("Protein,% alpha helix,% beta sheet\n", file = outfile, append = TRUE)
 
 # Iterate over every protein ID in infile.
 
 for (pid in pids)
 {
     # Get the PDB entry for this protein online.
-
     pdb <- read.pdb(pid)
 
     # Print the % alpha helix and % beta sheet
     # values based on the number and lengths of the
     # structures compared to the number of residues
     # in the sequence.
-
     helixpct <- sum(pdb$helix$end - pdb$helix$start) / length(pdb$seqres) * 100
     sheetpct <- sum(pdb$sheet$end - pdb$sheet$start) / length(pdb$seqres) * 100
 
-    formatted <- sprintf("%s,%.2f,%.2f,", pid, helixpct, sheetpct)
-
+    formatted <- sprintf("%s,%.2f,%.2f\n", pid, helixpct, sheetpct)
     cat(formatted, file = outfile, sep = "", append = TRUE)
 }
