ac6acfc4161ee2add7e46d3ab90a1c371ae8759c
Sam Pollard <sam.d.pollard@gmail.com>
2014-06-02 20:09:32 -0700

    Made several more plots, and colored all except exploratory plots. Now the plots save to their own directory

--- clustering.R
+++ clustering.R
@@ -2,7 +2,7 @@
 # Clusters the protein crystallization data in two areas:
 # Protein physical properties and crystallization conditions
 # Author: Sam Pollard (sam.d.pollard@gmail.com)
-# Last Modifed: May 28, 2014
+# Last Modifed: June 2, 2014
 #
 # Source code taken from Ellyn Ayton
 # (https://github.com/EllynAyton/Proteins/blob/master/colorTree.R)
@@ -26,37 +26,7 @@ datacols <- colnames(table)
 # 19 -> PEG #; 20 -> PEG %; 21 -> Ionic Strength; 22 -> PEG# * PEG% / 100
 # 23 -> GRAVY
 
-# NOTE: This may be done with the physical$diss which gives the dissimilarity
-# matrix, which in turn may be passed in to hclust and then tweaked using
-# methods outlined in
-# http://rstudio-pubs-static.s3.amazonaws.com/1876_df0bf890dd54461f98719b461d987c3d.html
-
-# Change the plot parameters
-plotsettings <- par(cex = 0.8, cex.main = 1.4, cex.sub = 0.9, cex.lab = 1.25,
-        cex.axis = 1.25, col.main = "blue", bg = "white",
-        col = "black")
-
-# Basic Physical Data:
-basic_cols <- c(1,8,10,11,16)
-physical_data_basic <- data.frame(cbind(table[basic_cols]),
-        row.names = 1, check.rows = TRUE)
-# agnes: average is UPGMA, "complete" is complete linkage, "ward", "weighted"
-physical_basic <- agnes(physical_data_basic, diss = FALSE, metric = "euclidean",
-      stand = TRUE, method = "average", keep.diss = TRUE, keep.data = TRUE)
-
-# Basic Physical Plot
-ptitlestr_b <- "Clustering of Various Proteins from Simple Physical Properties"
-psubtitlestr_b <- paste("Using ",
-        paste(datacols[basic_cols[2:length(basic_cols)]], collapse = ", "),
-        "(AC = ", round(physical_basic$ac, digits = 2), ")")
-pdf('trees/physical_cluster_basic.pdf')
-par(plotsettings)
-# which.plots is 1 for banner, 2 for dendrogram
-plot(physical_basic, which.plots = 2, main = paste(ptitlestr_b, " (agnes)"),
-        sub = psubtitlestr_b)
-exitval <- dev.off()
-
-# Make a tree with colored leaves
+# How to color leaves
 dnaColor <- "red"
 membraneColor <- "orange"
 enzymeColor <- "purple"
@@ -67,7 +37,6 @@ membrane <- c("1KN9", "3BF0", "4GWM", "3VMT", "2XCI", "2OLV", "4HHS", "1CX2", "4
 enzymes <- c("4GYR", "3I27", "3P1Z", "3V8J", "2HIV", "3L2P", "2NSF", "1FW1", "2Z6W", "4J4N")
 signaling <- c("2WXW", "1A28", "1ET1", "3TH5", "3TVD", "2CLS", "3KKV", "4FG7", "3RP9", "3RDJ")
 transport <- c("1PV6", "1PW4", "3O7Q", "3QS5", "3QS4", "3G61", "4PE6", "4FI3", "1F30", "1F2T")
-
 proteinGroups <- list(dnaBinding, membrane, enzymes, signaling, transport)
 colors <- c(dnaColor,membraneColor,enzymeColor,signalColor,transportColor)
 names(proteinGroups) <- colors
@@ -90,10 +59,40 @@ colorLabels <<- function(n, groups) {
     }
     n # I don't know what this does but it's in all the examples
 }
+
+# Change the plot parameters
+plotsettings <- par(cex = 0.8, cex.main = 1.4, cex.sub = 0.9, cex.lab = 1.25,
+        cex.axis = 1.25, col.main = "blue", bg = "white",
+        col = "black")
+
+# Creating and Plotting the data
+# NOTE: This may be done with the physical$diss which gives the dissimilarity
+# matrix, which in turn may be passed in to hclust and then tweaked using
+# methods outlined in
+# http://rstudio-pubs-static.s3.amazonaws.com/1876_df0bf890dd54461f98719b461d987c3d.html
+
+# Basic Physical Data:
+basic_cols <- c(1,8,10,11,16)
+physical_data_basic <- data.frame(cbind(table[basic_cols]),
+        row.names = 1, check.rows = TRUE)
+# agnes: average is UPGMA, "complete" is complete linkage, "ward", "weighted"
+physical_basic <- agnes(physical_data_basic, diss = FALSE, metric = "euclidean",
+      stand = TRUE, method = "average", keep.diss = TRUE, keep.data = TRUE)
+
+# Basic Physical Plot
+ptitlestr_b <- "Clustering of Various Proteins from Simple Physical Properties"
+psubtitlestr_b <- paste("Using ",
+        paste(datacols[basic_cols[2:length(basic_cols)]], collapse = ", "),
+        "(AC = ", round(physical_basic$ac, digits = 2), ")")
+
 hc <- hclust(physical_basic$diss, method = "average")
 hcd <- as.dendrogram(hc, hang = 0.05) # Hang so leaves are at different levels
 prettyTree <- dendrapply(hcd, colorLabels, proteinGroups)
-plot(prettyTree, main = ptitlestr_b, sub = psubtitlestr_b)
+pdf('trees/physical_cluster_basic_colored.pdf')
+par(plotsettings)
+plot(prettyTree, main = paste(ptitlestr_b, "(hclust)"), sub = psubtitlestr_b,
+        ylab = "height")
+exitval <- dev.off()
 # One can call str(prettytree) to see the heights of the trees
 # methods(class = class(prettytree)) to get the methods on a dendrogram
 
@@ -110,10 +109,13 @@ ptitlestr <- "Clustering of Various Proteins from Physical Properties"
 psubtitlestr <- paste("Using ",
         paste(datacols[physical_cols[2:length(physical_cols)]],
         collapse = ", "), "(AC = ", round(physical$ac, digits = 2), ")")
+hc <- hclust(physical$diss, method = "average")
+hcd <- as.dendrogram(hc, hang = 0.05)
+prettyTree <- dendrapply(hcd, colorLabels, proteinGroups)
 pdf('trees/physical_cluster.pdf')
 par(plotsettings)
-plot(physical, which.plots = 2, main = paste(ptitlestr, " (agnes)"),
-        sub = psubtitlestr)
+plot(prettyTree, main = paste(ptitlestr, "(hclust)"), sub = psubtitlestr,
+        ylab = "height")
 exitval <- dev.off()
 
 # Crystallization Data:
@@ -121,7 +123,7 @@ crystal_cols <- c(1,13,14,21,22)
 crystal_data <- data.frame(cbind(table[crystal_cols]),
         row.names = 1, check.rows = TRUE)
 crystal <- agnes(crystal_data, diss = FALSE, metric = "euclidean",
-        stand = TRUE, method = "average", keep.diss = FALSE, keep.data = TRUE)
+        stand = TRUE, method = "average", keep.diss = TRUE, keep.data = TRUE)
 
 # Crystallization Plot
 ctitlestr <- 
@@ -129,17 +131,40 @@ ctitlestr <-
 csubtitlestr <-  paste("Using ",
         paste(datacols[crystal_cols[2:length(crystal_cols)]], collapse = ", "),
         "(AC = ", round(crystal$ac, digits = 2), ")")
+hc <- hclust(crystal$diss, method = "average")
+hcd <- as.dendrogram(hc, hang = 0.05)
+prettyTree <- dendrapply(hcd, colorLabels, proteinGroups)
 pdf('trees/crystal_cluster.pdf')
 par(plotsettings)
-plot(crystal, which.plots = 2, main = ctitlestr, sub = csubtitlestr)
+plot(prettyTree, main = paste(ctitlestr, "(hclust)"), sub = csubtitlestr,
+        ylab = "height")
 exitval <- dev.off()
 
-# Alternative method of plotting using hclust
-#phc <- hclust(physical$diss, method = "average")
-# plot(phc, main = paste(ptitlestr, " (hclust)"),
-#         sub = "Using Molecular Weight, % helix, % sheet, Matthews Density.")
-# phcd <- as.dendrogram(phc)
-# plot(phcd, main = "Experimental Physical Dendrogram")
+# Plot of Everything
+comp_cols <- c(1,8,10,11,13,14,15,16,17,18,19,20,21,23)
+# Data:
+comp_data <- data.frame(cbind(table[comp_cols]), row.names = 1, check.rows=TRUE)
+comp <- agnes(comp_data, diss = FALSE, metric = "euclidean",
+        stand = TRUE, method = "average", keep.diss = TRUE, keep.data = TRUE)
+
+# Plot:
+comp_title <-
+        "Clustering of Various Proteins from All Data Available"
+comp_sub <- paste("Using ",
+            paste(datacols[comp_cols[2:length(comp_cols)]],
+                    collapse = ", "),
+            "(AC = ", round(comp$ac, digits = 2), ")")
+hc <- hclust(crystal$diss, method = "average")
+hcd <- as.dendrogram(hc, hang = 0.05)
+prettyTree <- dendrapply(hcd, colorLabels, proteinGroups)
+pdf('trees/comprehensive_cluster.pdf')
+# Change plot parameters so subtitle can fit
+par(cex = 0.8, cex.main = 1.4, cex.sub = 0.5, cex.lab = 1.25,
+        cex.axis = 1.25, col.main = "blue", bg = "white",
+        col = "black")
+plot(prettyTree, main = paste(comp_title, "(hclust)"), sub = comp_sub,
+        ylab = "height")
+exitval <- dev.off()
 
 # Exploratory (Experimental) plot(s)
 explore_1cols <- c(1,13,14,16,17,23)
@@ -147,7 +172,7 @@ explore_1cols <- c(1,13,14,16,17,23)
 explore_1data <- data.frame(cbind(table[explore_1cols]),
         row.names = 1, check.rows = TRUE)
 explore_1 <- agnes(explore_1data, diss = FALSE, metric = "euclidean",
-        stand = TRUE, method = "average", keep.diss = FALSE, keep.data = TRUE)
+        stand = TRUE, method = "average", keep.diss = TRUE, keep.data = TRUE)
 
 # Plot:
 explore_1title <- 
@@ -167,7 +192,7 @@ explore_2cols <- c(1,10,11,14,18,21,23)
 explore_2data <- data.frame(cbind(table[explore_2cols]),
         row.names = 1, check.rows = TRUE)
 explore_2 <- agnes(explore_2data, diss = FALSE, metric = "euclidean",
-        stand = TRUE, method = "average", keep.diss = FALSE, keep.data = TRUE)
+        stand = TRUE, method = "average", keep.diss = TRUE, keep.data = TRUE)
 
 # Plot:
 explore_2title <- 
